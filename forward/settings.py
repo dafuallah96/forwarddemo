dbdb08e2fb7f4cc3f538b3cf515ffda37ae1d9cf
dafuallah96 <dafuallah96@gmail.com>
2021-12-17 16:52:35 +0800

    this is new

--- forward/settings.py
+++ forward/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-&rnclw3qpsua_w83y0wf%jc5j3d)bd@(by*a1#8mjzp7s&k(wn
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
